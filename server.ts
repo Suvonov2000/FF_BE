a6777c21cc23510a76eed937771483c74a66d033
Suvonov2000 <suvonov200@gmail.com>
2025-10-08 15:38:39 +0900

    ff_be

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv";
 import {authRouter} from "./routes";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -12,6 +13,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use("/api/auth",authRouter)
 
-app.listen(process.env.PORT, ()=>{
+// mongoose.connect(process.env.MONGO_URL ||"" ).then (()=>{
+//     console.log("Connect to MongoDB");
+// }).catch((error)=>{
+//     console.log(error);
+    
+// })
+
+app.listen(process.env.PORT || 3000 , ()=>{
     console.log(`Server is started on port ${process.env.PORT || 3000}`); 
 })
